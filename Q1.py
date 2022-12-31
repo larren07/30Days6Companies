07f241957965aa3cd075949b9d37c0e3ea52cce9
larren07 <larren1234@gmail.com>
2022-12-31 13:42:59 +0530

    First Qt

--- Q1.py
+++ Q1.py
@@ -1,1 +1,27 @@
-print("Welcome")
+from ast import List
+
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        st = []
+        ops = ['+','-','*','/']
+        for i in tokens:
+            if i not in ops:
+                st.append(int(i))
+            else:
+                if(i == '+'):
+                    res = st.pop(-1) + st.pop(-1)
+                elif(i == '-'):
+                    a = st.pop(-1)
+                    b = st.pop(-1)
+                    res =  b - a
+                elif(i == '*'):
+                    res = st.pop(-1) * st.pop(-1)
+                else:
+                    a = st.pop(-1)
+                    b = st.pop(-1)
+                    res =  int(b /a)
+
+                st.append(res)
+            # print(st)
+        return st[0]
